b4545096cf10bab5a8205a9e8791ae3beb58e991
zhangji261 <523540654@qq.com>
2025-08-26 14:31:19 +0800

    添加Spring Boot应用代码和Maven配置

---
+++ src/main/java/com/demo/controller/HelloController.java
@@ -0,0 +1,18 @@
+package com.demo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello, GitHub Tools Demo!";
+    }
+    
+    @GetMapping("/api/status")
+    public String getStatus() {
+        return "Service is running";
+    }
+}
